c9a33d036222a6821f1e34db9ed1e0d760b9691a
chen_glb <597879949@qq.com>
2022-10-08 23:38:24 +0800

    demo merge

--- shiro/src/main/java/com/yy/ShiroRun.java
+++ shiro/src/main/java/com/yy/ShiroRun.java
@@ -1,21 +1,12 @@
 package com.yy;
 
 import org.apache.shiro.SecurityUtils;
-import org.apache.shiro.authc.AuthenticationException;
-import org.apache.shiro.authc.AuthenticationToken;
-import org.apache.shiro.authc.IncorrectCredentialsException;
-import org.apache.shiro.authc.UnknownAccountException;
-import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.authc.*;
 import org.apache.shiro.config.Ini;
 import org.apache.shiro.config.IniSecurityManagerFactory;
 import org.apache.shiro.mgt.SecurityManager;
 import org.apache.shiro.subject.Subject;
 
-/**
- * @Author: chen
- * @Date: 2022/9/28 16:28
- * @Desc:
- */
 public class ShiroRun {
     public static void main(String[] args) {
         Ini ini = IniSecurityManagerFactory.loadDefaultClassPathIni();
@@ -28,28 +19,27 @@ public class ShiroRun {
 
         Subject subject = SecurityUtils.getSubject();
 
-        AuthenticationToken token = new UsernamePasswordToken("zhangsan",
-                                                              "z3");
+        AuthenticationToken token = new UsernamePasswordToken("zhangsan", "z3");
         try {
+            // 登入认证
             subject.login(token);
 
+            // 判断角色
             System.out.println(subject.hasRole("role1"));
+            // 判断权限
             System.out.println(subject.isPermitted("user:insert"));
+            // checkPermission没有返回值,如果没有对应的权限则直接抛出异常
+            subject.checkPermission("user:select");
 
             System.out.println("登录成功");
-        }
-        catch (UnknownAccountException e) {
+        } catch (UnknownAccountException e) {
             e.printStackTrace();
             System.out.println("用户不存在");
-        }
-        catch (IncorrectCredentialsException e) {
+        } catch (IncorrectCredentialsException e) {
             e.printStackTrace();
             System.out.println("密码错误");
-        }
-        catch (AuthenticationException ae) {
+        } catch (AuthenticationException ae) {
             ae.printStackTrace();
         }
-
-
     }
 }
